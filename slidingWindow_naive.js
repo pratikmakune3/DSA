658d148bf8e478330149694dd4e186f4996c4c19
pratikmakune3 <pratikm279@gmail.com>
2020-07-28 18:57:29 +0530

    Added: Edge Case

--- slidingWindow_naive.js
+++ slidingWindow_naive.js
@@ -1,21 +1,29 @@
-let arr = [-2,4,-1,4,-3,2,-6,3];
-
-let num = 4;
-
-// To support negating array elements also
-let maxSum = -Infinity;
-
-for(let i=0; i<(arr.length - num + 1); i++){
-  let tempSum = 0;
-  for(let j=i; j<i+num; j++){
-    console.log(arr[j]);
-    tempSum += arr[j];
+function maxSubarraySum(arr, num) {
+  // Edge case
+  if(arr.length < num){
+    return null;
   }
-  console.log('sum',tempSum);
-  console.log('---------');
-  if(tempSum > maxSum){
-    maxSum = tempSum;
+  // To support negating array elements also
+  let maxSum = -Infinity;
+
+  for(let i=0; i<(arr.length - num + 1); i++){
+    let tempSum = 0;
+    for(let j=i; j<i+num; j++){
+      console.log(arr[j]);
+      tempSum += arr[j];
+    }
+    console.log('sum',tempSum);
+    console.log('---------');
+    if(tempSum > maxSum){
+      maxSum = tempSum;
+    }
   }
+
+  return maxSum;
 }
 
-console.log('maxSum-->',maxSum)
+maxSubarraySum([2,4,1,4,3,2,6,3], 4); // Should return 15
+
+maxSubarraySum([-2,4,-1,4,-3,2,-6,3], 4); // Should return 5
+
+maxSubarraySum([-2,4], 4); // Should return null
