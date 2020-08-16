12a1fd6ac13660ce0a8762b70f317a2a306b0b42
pratikmakune3 <pratikm279@gmail.com>
2020-08-16 18:53:12 +0530

    Added: Stack implementation using Linked List

---
+++ Stack.js
@@ -0,0 +1,53 @@
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack{
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  
+  push(val){
+    let newNode = new Node(val);
+
+    if(!this.first){
+      this.first = newNode;
+      this.last = newNode;
+    }else{
+      let temp = this.first;
+      this.first = newNode;
+      newNode.next = temp;
+    }
+
+    return ++this.length;
+  }
+
+  pop(){
+    if(!this.first) return null;
+    let temp = this.first;
+
+    if(this.length === 1){
+      this.first = null;
+      this.last = null;
+    }else{
+      this.first = this.first.next;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+}
+
+let stack = new Stack();
+console.log(stack.push(10));
+console.log(stack.push(20));
+console.log(stack.push(30));
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack);
