1fe5e055866db62b0afd177443b65abfdbde49f9
pratikmakune3 <pratikm279@gmail.com>
2020-09-26 20:06:32 +0530

    Updated: MaxHeap implementation

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -1,11 +1,13 @@
 class MaxBinaryHeap {
   constructor(){
-    this.values = [41, 39, 33, 18, 27, 12];
+    this.values = [41, 39, 33, 18, 27, 12, 6, 7];
   }
+  
   insert(element){
     this.values.push(element);
     this.bubbleUp(element);
   }
+  
   bubbleUp(){
     let elementIdx = this.values.length - 1;
 
@@ -26,8 +28,49 @@ class MaxBinaryHeap {
       elementIdx = parentIdx;
     }
   }
+
+  extractMax(){
+    // [41, 39, 33, 18, 27, 12, 7, 6];
+    // [6, 39, 33, 18, 27, 12, 7];
+    this.values[0] = this.values.pop();
+    let elemIndex = 0;
+    let arrayLength = this.values.length;
+
+    while(true){
+      let elem = this.values[elemIndex];
+      
+      let leftChildIndex = 2*elemIndex + 1;
+      let leftChild = this.values[leftChildIndex];
+  
+      let rightChildIndex = 2*elemIndex + 2;
+      let rightChild = this.values[rightChildIndex];
+  
+      if(leftChildIndex < arrayLength && rightChildIndex < arrayLength ){
+        if(leftChild < rightChild){
+          this.swap(elemIndex, rightChildIndex)
+          elemIndex = rightChildIndex;
+        }else{
+          this.swap(elemIndex, leftChildIndex)
+          elemIndex = leftChildIndex;
+        }
+      }else if(leftChildIndex < arrayLength && rightChildIndex >= arrayLength && elem < this.values[leftChildIndex]){
+        this.swap(elemIndex, leftChildIndex)
+        elemIndex = leftChildIndex;
+      }else{
+        break;
+      }
+    }
+  }
+
+  swap(num1, num2){
+    let temp = this.values[num1];
+    this.values[num1] = this.values[num2];
+    this.values[num2] = temp;
+  }
+
 }
 
 let heap = new MaxBinaryHeap();
-heap.insert(5);
+heap.extractMax();
+// heap.extractMax();
 console.log(heap)
