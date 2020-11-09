b365964ca0d6e67d12bc1feec721b94f78ba53a9
pratikmakune3 <pratikm279@gmail.com>
2020-11-09 13:55:07 +0530

    Optimised: Bubble sorting for already sorted array

--- Sorting-bubble.js
+++ Sorting-bubble.js
@@ -1,16 +1,20 @@
-// 2,7,5,4,1 
-// i=0  2,5,7,4,1
-// i=1  2,5,4,7,  
-
 function bubbleSort(arr){
   for(let i=0; i<arr.length; i++){
+    let isSwapped = false;
     for(let j=0; j<arr.length-i; j++){
       if(arr[j]>arr[j+1]){
+        // Swap
         let temp=arr[j];
         arr[j]=arr[j+1];
         arr[j+1]=temp;
+
+        isSwapped = true;
       }
     }
+    if(!isSwapped){
+      console.log('isSwapped false')
+      break;
+    } 
   }
   return arr;
 }
