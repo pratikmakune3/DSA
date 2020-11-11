9c2dcc68fa2e931da8258c13861fc5c04b674a4d
pratikmakune3 <pratikm279@gmail.com>
2020-11-11 21:51:20 +0530

    Added: Check for sorted in non decreasing order

---
+++ Array-checkSortedNotDecreasing.js
@@ -0,0 +1,10 @@
+function isArraySorted(arr){
+  for(let i=0; i<arr.length-1; i++){
+    if(!(arr[i] <= arr[i+1])){
+      return false
+    }
+  }
+  return true;
+}
+
+console.log(isArraySorted([5,10,10,15]))
